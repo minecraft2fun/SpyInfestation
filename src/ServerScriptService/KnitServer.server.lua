0d0d20a75bb834003083d069a442a22313f7cc92
minecraft2fun <73174685+minecraft2fun@users.noreply.github.com>
2023-02-07 13:54:05 +0000

    CameraService

--- src/ServerScriptService/KnitServer.server.lua
+++ src/ServerScriptService/KnitServer.server.lua
@@ -1,22 +1,44 @@
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
 local Knit = require(ReplicatedStorage.Packages.Knit)
+local TableUtil = require("ReplicatedStorage.Packages.TableUtil")
 
-local MoneyService = Knit.CreateService{
-    Name = "MoneyService",
-    Client = {}
+local CameraService = Knit.CreateService{
+    Name = "CameraService",
+    Client = {},
+    RunningCameras = {}
 }
 
-function MoneyService:GetMoney(player : Player)
+function CameraService:GetMoney(player : Player)
     return 0
 end
-
-function MoneyService:KnitStart()
+function CameraService:StartCamera(camera:Folder)
     
 end
 
-function MoneyService:KnitInit()
+function CameraService:KillCamera(camera)
+local function run(fold)
     
+end
+
+
+    if camera:IsA("Folder") then
+        run(camera)
+        --singluar
+    elseif camera:IsA("Table") then
+        --loop through, Kill
+        for _,v in pairs(camera) do
+            run(v)
+        end
+    end
+end
+
+function CameraService:KnitStart()
+    
+end
+
+function CameraService:KnitInit()
+    print("Starting Knit Service "..script.Name)
 end
 
 Knit.Start():andThen(function()
